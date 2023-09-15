806ba2941c4137877b555503e0269fe9d5f12e03
Tricia Ball <trish.ball@gmail.com>
2023-09-14 23:29:51 -0500

    Update to match Field Centric Drive on GM0
    See:
    https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html#robot-centric-final-sample-code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDriveChatGPT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDriveChatGPT.java
@@ -1,47 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
-
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 // @Disabled
 @TeleOp(name="FieldCentricDrive", group="Sample")
 public class FieldCentricDriveChatGPT extends LinearOpMode {
-
-    private DcMotor frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive;
-    private IMU imu;
-
     @Override
     public void runOpMode() throws InterruptedException {
-        // Initialize hardware
-        frontLeftDrive  = hardwareMap.dcMotor.get("frontLeftDrive");
-        frontRightDrive = hardwareMap.dcMotor.get("frontRightDrive");
-        backLeftDrive   = hardwareMap.dcMotor.get("backLeftDrive");
-        backRightDrive  = hardwareMap.dcMotor.get("backRightDrive");
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map
-        imu = hardwareMap.get(IMU.class, "imu");
+        IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        // Initialize the IMU
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
         waitForStart();
@@ -49,9 +41,9 @@ public class FieldCentricDriveChatGPT extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double drive = -gamepad1.left_stick_y;  // Reverse this to change the direction of forward movement
-            double strafe = gamepad1.left_stick_x;
-            double rotate = gamepad1.right_stick_x;
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
@@ -60,37 +52,27 @@ public class FieldCentricDriveChatGPT extends LinearOpMode {
                 imu.resetYaw();
             }
 
-            // Get the current robot orientation
-            Orientation angles   = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-            double robotAngle = Math.toRadians(angles.firstAngle);  // Convert to radians
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-            // Field-centric calculations
-            double temp = drive * Math.cos(robotAngle) - strafe * Math.sin(robotAngle);
-            strafe = drive * Math.sin(robotAngle) + strafe * Math.cos(robotAngle);
-            drive = temp;
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
 
-            // Calculate powers for each wheel
-            double frontLeftPower = drive + strafe + rotate;
-            double frontRightPower = drive - strafe - rotate;
-            double backLeftPower = drive - strafe + rotate;
-            double backRightPower = drive + strafe - rotate;
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
 
-            // Normalize powers so no value exceeds 1.0
-            double maxPower = Math.max(Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower)),
-                    Math.max(Math.abs(backLeftPower), Math.abs(backRightPower)));
-            if (maxPower > 1.0) {
-                frontLeftPower /= maxPower;
-                frontRightPower /= maxPower;
-                backLeftPower /= maxPower;
-                backRightPower /= maxPower;
-            }
-
-            // Set motor powers
-            frontLeftDrive.setPower(frontLeftPower);
-            frontRightDrive.setPower(frontRightPower);
-            backLeftDrive.setPower(backLeftPower);
-            backRightDrive.setPower(backRightPower);
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
         }
     }
 }
