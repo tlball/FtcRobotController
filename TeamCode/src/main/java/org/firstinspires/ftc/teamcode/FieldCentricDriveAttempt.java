0009c676822b0283dc14ccabed094f2e865b37d1
Tricia Ball <trish.ball@gmail.com>
2023-09-14 23:37:26 -0500

    Rename class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDriveAttempt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDriveAttempt.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 // @Disabled
 @TeleOp(name="FieldCentricDrive", group="Sample")
-public class FieldCentricDriveChatGPT extends LinearOpMode {
+public class FieldCentricDriveAttempt extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
